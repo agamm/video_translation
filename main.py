a140246510dde06bebd4bbe2d3b8a10854cbaf91
Agam More <agam@agam.me>
2023-10-15 01:23:15 +0400

    Add comments to the main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import csv
 from moviepy.editor import VideoFileClip, CompositeVideoClip
 from moviepy.video.VideoClip import TextClip
 
-# Define your input file paths and language
-MP4_FILE = "./video.mp4"
-CSV_FILE = "./translations.csv"
-LANGUAGE = "Russian"
-SUB_POS = "bottom"
-BOX_TOP = 0.15
-BOX_BOT = 0.24
+# Define your inputs
+MP4_FILE = "./video.mp4"  # Base file to add subtitles to
+CSV_FILE = "./translations.csv"  # CSV translation
+LANGUAGE = "English"  # What language to add subtitles for (via csv)
+SUB_POS = "bottom"  # Subtitle position
+BOX_TOP = 0.15  # Letterbox top size %
+BOX_BOT = 0.24  # Letterbox bottom size %
 TEXT_SIZE = 20  # 16 = big, 20=small (higher = smaller)
 
 
@@ -20,16 +20,15 @@ def process_video(mp4_path, csv_path, language):
             subtitles.append(row)
 
     video_clip = VideoFileClip(mp4_path)
-    # Define the size of the letterbox
 
-    def my_filter(frame):
+    def letterbox_filter(frame):
         """Blacken a rectangular zone of the frame"""
         size_t = int(video_clip.h*BOX_TOP)
         size_b = int(video_clip.h*BOX_BOT)
         frame[:size_t, :] = 0
         frame[-size_b:, :] = 0
         return frame
-    letterboxed = CompositeVideoClip([video_clip]).fl_image(my_filter)
+    letterboxed = CompositeVideoClip([video_clip]).fl_image(letterbox_filter)
 
     final_clip = add_subtitles_to_video(language, subtitles, letterboxed)
 
@@ -76,7 +75,7 @@ def add_subtitles_to_video(language, subtitles, video_clip):
         text = subtitles[2][i-1]
 
         text = add_newline_if_long(text)
-        print(text)
+        print(start_time, text)
 
         subtitle_clip = TextClip(
             text, fontsize=video_clip.h // TEXT_SIZE, color='white', bg_color='black', font="Helvetica")
@@ -88,6 +87,7 @@ def add_subtitles_to_video(language, subtitles, video_clip):
             pos = 0.88
         subtitle_clip = subtitle_clip.set_position(
             ("center", pos), relative=True)
+
         subtitle_clips.append(subtitle_clip)
 
     final_clip = CompositeVideoClip([video_clip] + subtitle_clips)
